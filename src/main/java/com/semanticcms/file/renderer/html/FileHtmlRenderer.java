796a0088fd2d42625475aff9e3e7c4766884ad82
AO Industries, Inc <support@aoindustries.com>
2022-04-20 04:58:23 -0500

    Significant manual code formatting clean-up…
    … in preparation for an automatic conversion to Google Java Style Guide:
    
    1) Added braces around if/else/for/while where missing
    
    2) Indentation is now 2-space, all file formats
    
      a) HTML indentation changed from tab to 2-space
    
      b) XML indentation changed from 4-space to 2-space
    
    3) Continuation is 4-space for JSON, Java, and JavaScript
    
    4) Updated NetBeans, kwrite, and kate settings

--- src/main/java/com/semanticcms/file/renderer/html/FileHtmlRenderer.java
+++ src/main/java/com/semanticcms/file/renderer/html/FileHtmlRenderer.java
@@ -53,175 +53,185 @@ import javax.servlet.jsp.SkipPageException;
 
 public final class FileHtmlRenderer {
 
-	/** Make no instances. */
-	private FileHtmlRenderer() {throw new AssertionError();}
+  /** Make no instances. */
+  private FileHtmlRenderer() {
+    throw new AssertionError();
+  }
 
-	/**
-	 * @param  <Ex>  An arbitrary exception type that may be thrown
-	 */
-	@FunctionalInterface
-	public static interface FileImplBody<Ex extends Throwable> {
-		void doBody(boolean discard) throws Ex, IOException, SkipPageException;
-	}
+  /**
+   * @param  <Ex>  An arbitrary exception type that may be thrown
+   */
+  @FunctionalInterface
+  public static interface FileImplBody<Ex extends Throwable> {
+    void doBody(boolean discard) throws Ex, IOException, SkipPageException;
+  }
 
-	/**
-	 * @param content Optional, when null meta data is verified but no output is generated
-	 */
-	public static void writeFileImpl(
-		ServletContext servletContext,
-		HttpServletRequest request,
-		HttpServletResponse response,
-		AnyUnion_Palpable_Phrasing<?, ?> content,
-		com.semanticcms.file.model.File element
-	) throws ServletException, IOException {
-		ResourceStore resourceStore;
-		ResourceRef resourceRef;
-		{
-			Tuple2<ResourceStore, ResourceRef> resource = element.getResource();
-			if(resource == null) throw new IllegalArgumentException("Resource not set on file: " + element);
-			resourceStore = resource.getElement1();
-			resourceRef = resource.getElement2();
-		}
-		BookRef bookRef = resourceRef.getBookRef();
-		// Find the resource, if available
-		Resource resource = resourceStore == null ? null : resourceStore.getResource(resourceRef.getPath());
-		// Connect to resource
-		ResourceConnection conn = resource == null ? null : resource.open();
-		try {
-			// Find the local file, if available
-			File resourceFile; {
-				File resourceFile_;
-				if(conn == null || !conn.exists()) {
-					resourceFile_ = null;
-				} else {
-					assert resource != null;
-					try {
-						resourceFile_ = resource.getFile();
-					} catch(FileNotFoundException e) {
-						// Resource removed between exists() and getFile()
-						resourceFile_ = null;
-					}
-				}
-				resourceFile = resourceFile_;
-			}
-			// Check if is directory and filename matches required pattern for directory
-			boolean isDirectory;
-			if(resourceFile == null) {
-				// In other book and not available, assume directory when ends in path separator
-				isDirectory = resourceRef.getPath().toString().endsWith(Path.SEPARATOR_STRING);
-			} else {
-				// In accessible book, use attributes
-				isDirectory = resourceFile.isDirectory();
-				// When is a directory, must end in slash
-				if(
-					isDirectory
-					&& !resourceRef.getPath().toString().endsWith(Path.SEPARATOR_STRING)
-				) {
-					throw new IllegalArgumentException(
-						"References to directories must end in slash ("
-						+ Path.SEPARATOR_CHAR
-						+ "): "
-						+ resourceRef
-					);
-				}
-			}
-			if(content != null) {
-				BufferResult body = element.getBody();
-				boolean hasBody = body.getLength() != 0;
-				// Determine if local file opening is allowed
-				final boolean isOpenFileAllowed = FileUtils.isOpenFileAllowed(servletContext, request);
-				final boolean isExporting = Headers.isExporting(request);
+  /**
+   * @param content Optional, when null meta data is verified but no output is generated
+   */
+  public static void writeFileImpl(
+    ServletContext servletContext,
+    HttpServletRequest request,
+    HttpServletResponse response,
+    AnyUnion_Palpable_Phrasing<?, ?> content,
+    com.semanticcms.file.model.File element
+  ) throws ServletException, IOException {
+    ResourceStore resourceStore;
+    ResourceRef resourceRef;
+    {
+      Tuple2<ResourceStore, ResourceRef> resource = element.getResource();
+      if (resource == null) {
+        throw new IllegalArgumentException("Resource not set on file: " + element);
+      }
+      resourceStore = resource.getElement1();
+      resourceRef = resource.getElement2();
+    }
+    BookRef bookRef = resourceRef.getBookRef();
+    // Find the resource, if available
+    Resource resource = resourceStore == null ? null : resourceStore.getResource(resourceRef.getPath());
+    // Connect to resource
+    ResourceConnection conn = resource == null ? null : resource.open();
+    try {
+      // Find the local file, if available
+      File resourceFile; {
+        File resourceFile_;
+        if (conn == null || !conn.exists()) {
+          resourceFile_ = null;
+        } else {
+          assert resource != null;
+          try {
+            resourceFile_ = resource.getFile();
+          } catch (FileNotFoundException e) {
+            // Resource removed between exists() and getFile()
+            resourceFile_ = null;
+          }
+        }
+        resourceFile = resourceFile_;
+      }
+      // Check if is directory and filename matches required pattern for directory
+      boolean isDirectory;
+      if (resourceFile == null) {
+        // In other book and not available, assume directory when ends in path separator
+        isDirectory = resourceRef.getPath().toString().endsWith(Path.SEPARATOR_STRING);
+      } else {
+        // In accessible book, use attributes
+        isDirectory = resourceFile.isDirectory();
+        // When is a directory, must end in slash
+        if (
+          isDirectory
+          && !resourceRef.getPath().toString().endsWith(Path.SEPARATOR_STRING)
+        ) {
+          throw new IllegalArgumentException(
+            "References to directories must end in slash ("
+            + Path.SEPARATOR_CHAR
+            + "): "
+            + resourceRef
+          );
+        }
+      }
+      if (content != null) {
+        BufferResult body = element.getBody();
+        boolean hasBody = body.getLength() != 0;
+        // Determine if local file opening is allowed
+        final boolean isOpenFileAllowed = FileUtils.isOpenFileAllowed(servletContext, request);
+        final boolean isExporting = Headers.isExporting(request);
 
-				String elemId = element.getId();
-				AnyA<?, ? extends AnyUnion_Palpable_Phrasing<?, ?>, ?, ?> a = content.a();
-				if(elemId != null) {
-					// TODO: To appendIdInPage, review other uses, too
-					a.id(PageIndex.getRefIdInPage(request, element.getPage(), elemId));
-				}
-				if(!hasBody) {
-					// TODO: Class like core:link, where providing empty class disables automatic class selection here
-					a.clazz(HtmlRenderer.getInstance(servletContext).getLinkCssClass(element));
-				}
-				if(
-					isOpenFileAllowed
-					&& resourceFile != null
-					&& !isExporting
-				) {
-					a.href(response.encodeURL(resourceFile.toURI().toASCIIString()));
-				} else {
-					final String urlPath;
-					long lastModified;
-					if(
-						conn != null
-						&& !isDirectory
-						// Check for header disabling auto last modified
-						&& !"false".equalsIgnoreCase(request.getHeader(LastModifiedServlet.LAST_MODIFIED_HEADER_NAME))
-						&& conn.exists()
-						&& (lastModified = conn.getLastModified()) != 0
-					) {
-						// Include last modified on file
-						urlPath =
-							request.getContextPath()
-							+ bookRef.getPrefix()
-							+ resourceRef.getPath()
-							+ "?" + LastModifiedServlet.LAST_MODIFIED_PARAMETER_NAME
-							+ "=" + LastModifiedServlet.encodeLastModified(lastModified)
-						;
-					} else {
-						urlPath =
-							request.getContextPath()
-							+ bookRef.getPrefix()
-							+ resourceRef.getPath()
-						;
-					}
-					a.href(response.encodeURL(URIEncoder.encodeURI(urlPath)));
-				}
-				if(
-					isOpenFileAllowed
-					&& resourceFile != null
-					&& !isExporting
-				) {
-					a.onclick(onclick -> onclick
-						.append("semanticcms_openfile_servlet.openFile(").text(bookRef.getDomain()).append(", ").text(bookRef.getPath()).append(", ").text(resourceRef.getPath()).append("); return false;")
-					);
-				}
-				a.__(a__ -> {
-					if(!hasBody) {
-						if(resourceFile == null) {
-							String path = resourceRef.getPath().toString();
-							int slashBefore;
-							if(path.endsWith(Path.SEPARATOR_STRING)) {
-								slashBefore = path.lastIndexOf(Path.SEPARATOR_STRING, path.length() - 2);
-							} else {
-								slashBefore = path.lastIndexOf(Path.SEPARATOR_STRING);
-							}
-							String filename = path.substring(slashBefore + 1);
-							if(filename.isEmpty()) throw new IllegalArgumentException("Invalid filename for file: " + path);
-							a__.text(filename);
-						} else {
-							a__.text(resourceFile.getName());
-							if(isDirectory) a__.text(Path.SEPARATOR_CHAR);
-						}
-					} else {
-						@SuppressWarnings("deprecation")
-						Writer unsafe = a__.getRawUnsafe();
-						body.writeTo(new NodeBodyWriter(element, unsafe, new ServletElementContext(servletContext, request, response)));
-					}
-				});
-				long length;
-				if(
-					!hasBody
-					&& conn != null
-					&& !isDirectory
-					&& conn.exists()
-					&& (length = conn.getLength()) != -1
-				) {
-					content.text(" (").text(Strings.getApproximateSize(length)).text(')');
-				}
-			}
-		} finally {
-			// TODO: Close earlier?
-			if(conn != null) conn.close();
-		}
-	}
+        String elemId = element.getId();
+        AnyA<?, ? extends AnyUnion_Palpable_Phrasing<?, ?>, ?, ?> a = content.a();
+        if (elemId != null) {
+          // TODO: To appendIdInPage, review other uses, too
+          a.id(PageIndex.getRefIdInPage(request, element.getPage(), elemId));
+        }
+        if (!hasBody) {
+          // TODO: Class like core:link, where providing empty class disables automatic class selection here
+          a.clazz(HtmlRenderer.getInstance(servletContext).getLinkCssClass(element));
+        }
+        if (
+          isOpenFileAllowed
+          && resourceFile != null
+          && !isExporting
+        ) {
+          a.href(response.encodeURL(resourceFile.toURI().toASCIIString()));
+        } else {
+          final String urlPath;
+          long lastModified;
+          if (
+            conn != null
+            && !isDirectory
+            // Check for header disabling auto last modified
+            && !"false".equalsIgnoreCase(request.getHeader(LastModifiedServlet.LAST_MODIFIED_HEADER_NAME))
+            && conn.exists()
+            && (lastModified = conn.getLastModified()) != 0
+          ) {
+            // Include last modified on file
+            urlPath =
+              request.getContextPath()
+              + bookRef.getPrefix()
+              + resourceRef.getPath()
+              + "?" + LastModifiedServlet.LAST_MODIFIED_PARAMETER_NAME
+              + "=" + LastModifiedServlet.encodeLastModified(lastModified)
+            ;
+          } else {
+            urlPath =
+              request.getContextPath()
+              + bookRef.getPrefix()
+              + resourceRef.getPath()
+            ;
+          }
+          a.href(response.encodeURL(URIEncoder.encodeURI(urlPath)));
+        }
+        if (
+          isOpenFileAllowed
+          && resourceFile != null
+          && !isExporting
+        ) {
+          a.onclick(onclick -> onclick
+            .append("semanticcms_openfile_servlet.openFile(").text(bookRef.getDomain()).append(", ").text(bookRef.getPath()).append(", ").text(resourceRef.getPath()).append("); return false;")
+          );
+        }
+        a.__(a__ -> {
+          if (!hasBody) {
+            if (resourceFile == null) {
+              String path = resourceRef.getPath().toString();
+              int slashBefore;
+              if (path.endsWith(Path.SEPARATOR_STRING)) {
+                slashBefore = path.lastIndexOf(Path.SEPARATOR_STRING, path.length() - 2);
+              } else {
+                slashBefore = path.lastIndexOf(Path.SEPARATOR_STRING);
+              }
+              String filename = path.substring(slashBefore + 1);
+              if (filename.isEmpty()) {
+                throw new IllegalArgumentException("Invalid filename for file: " + path);
+              }
+              a__.text(filename);
+            } else {
+              a__.text(resourceFile.getName());
+              if (isDirectory) {
+                a__.text(Path.SEPARATOR_CHAR);
+              }
+            }
+          } else {
+            @SuppressWarnings("deprecation")
+            Writer unsafe = a__.getRawUnsafe();
+            body.writeTo(new NodeBodyWriter(element, unsafe, new ServletElementContext(servletContext, request, response)));
+          }
+        });
+        long length;
+        if (
+          !hasBody
+          && conn != null
+          && !isDirectory
+          && conn.exists()
+          && (length = conn.getLength()) != -1
+        ) {
+          content.text(" (").text(Strings.getApproximateSize(length)).text(')');
+        }
+      }
+    } finally {
+      // TODO: Close earlier?
+      if (conn != null) {
+        conn.close();
+      }
+    }
+  }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-file-renderer-html - Files referenced in HTML in a Servlet environment.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -21,27 +21,27 @@
  * along with semanticcms-file-renderer-html.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.semanticcms.file.renderer.html {
-	exports com.semanticcms.file.renderer.html;
-	// Direct
-	requires com.aoapps.encoding; // <groupId>com.aoapps</groupId><artifactId>ao-encoding</artifactId>
-	requires com.aoapps.html.any; // <groupId>com.aoapps</groupId><artifactId>ao-fluent-html-any</artifactId>
-	requires com.aoapps.hodgepodge; // <groupId>com.aoapps</groupId><artifactId>ao-hodgepodge</artifactId>
-	requires com.aoapps.io.buffer; // <groupId>com.aoapps</groupId><artifactId>ao-io-buffer</artifactId>
-	requires com.aoapps.lang; // <groupId>com.aoapps</groupId><artifactId>ao-lang</artifactId>
-	requires com.aoapps.net.types; // <groupId>com.aoapps</groupId><artifactId>ao-net-types</artifactId>
-	requires com.aoapps.servlet.lastmodified; // <groupId>com.aoapps</groupId><artifactId>ao-servlet-last-modified</artifactId>
-	requires javax.servlet.api; // <groupId>javax.servlet</groupId><artifactId>javax.servlet-api</artifactId>
-	requires javax.servlet.jsp.api; // <groupId>javax.servlet.jsp</groupId><artifactId>javax.servlet.jsp-api</artifactId>
-	requires com.semanticcms.core.controller; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-controller</artifactId>
-	requires com.semanticcms.core.model; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-model</artifactId>
-	requires com.semanticcms.core.pages; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-pages</artifactId>
-	requires com.semanticcms.core.renderer.html; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-renderer-html</artifactId>
-	requires com.semanticcms.core.resources; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-resources</artifactId>
-	requires com.semanticcms.core.servlet; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-servlet</artifactId>
-	requires com.semanticcms.file.model; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-file-model</artifactId>
-	requires static com.semanticcms.openfile.servlet; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-openfile-servlet</artifactId>
-	// Transitive
-	requires com.semanticcms.core.renderer; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-renderer</artifactId>
-	// Java SE
-	requires java.logging;
+  exports com.semanticcms.file.renderer.html;
+  // Direct
+  requires com.aoapps.encoding; // <groupId>com.aoapps</groupId><artifactId>ao-encoding</artifactId>
+  requires com.aoapps.html.any; // <groupId>com.aoapps</groupId><artifactId>ao-fluent-html-any</artifactId>
+  requires com.aoapps.hodgepodge; // <groupId>com.aoapps</groupId><artifactId>ao-hodgepodge</artifactId>
+  requires com.aoapps.io.buffer; // <groupId>com.aoapps</groupId><artifactId>ao-io-buffer</artifactId>
+  requires com.aoapps.lang; // <groupId>com.aoapps</groupId><artifactId>ao-lang</artifactId>
+  requires com.aoapps.net.types; // <groupId>com.aoapps</groupId><artifactId>ao-net-types</artifactId>
+  requires com.aoapps.servlet.lastmodified; // <groupId>com.aoapps</groupId><artifactId>ao-servlet-last-modified</artifactId>
+  requires javax.servlet.api; // <groupId>javax.servlet</groupId><artifactId>javax.servlet-api</artifactId>
+  requires javax.servlet.jsp.api; // <groupId>javax.servlet.jsp</groupId><artifactId>javax.servlet.jsp-api</artifactId>
+  requires com.semanticcms.core.controller; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-controller</artifactId>
+  requires com.semanticcms.core.model; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-model</artifactId>
+  requires com.semanticcms.core.pages; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-pages</artifactId>
+  requires com.semanticcms.core.renderer.html; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-renderer-html</artifactId>
+  requires com.semanticcms.core.resources; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-resources</artifactId>
+  requires com.semanticcms.core.servlet; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-servlet</artifactId>
+  requires com.semanticcms.file.model; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-file-model</artifactId>
+  requires static com.semanticcms.openfile.servlet; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-openfile-servlet</artifactId>
+  // Transitive
+  requires com.semanticcms.core.renderer; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-renderer</artifactId>
+  // Java SE
+  requires java.logging;
 }
